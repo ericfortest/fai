bf0171fda69263785cf75ea62fd467ebac8c3021
ericfortest <148351524+ericfortest@users.noreply.github.com>
2023-10-25 13:53:26 +0200

    Add files via upload
    added a script to set Chrome, Files, Settings, Terminal, LibreOffice Writer as favorite-apps

---
+++ favorite.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo -ne '\n' | gsettings set org.gnome.shell favorite-apps "['google-chrome.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', 'gnome-control-center.desktop', 'libreoffice-writer.desktop']"
+
